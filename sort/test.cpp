1ccc2ba44ffec441f538a5d8846aa9377ea2e2d9
Ashwin Abraham <210050023@iitb.ac.in>
2022-12-28 14:38:12 +0530

    Added multithreaded searching and sorting algorithms

--- sort/test.cpp
+++ sort/test.cpp
@@ -3,7 +3,7 @@
 #include <chrono>
 #include "sort.hpp"
 
-constexpr std::size_t SIZE = 1'000'000'000;
+constexpr std::size_t SIZE = 100'000'000;
 constexpr std::size_t RANGE = 3001;
 
 int main(int argc, char ** argv)
@@ -11,7 +11,7 @@ int main(int argc, char ** argv)
     int *arr = new int[SIZE];
     for(int i = 0; i < SIZE; ++i) arr[i] = rand() % RANGE;
     auto t1 = std::chrono::high_resolution_clock::now();
-    sort::threadsearch(arr, arr[3*SIZE/4], SIZE);
+    sort::bubblesort(arr, SIZE);
     auto t2 = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration<double, std::milli>(t2 - t1).count() << '\n';
     delete [] arr;
