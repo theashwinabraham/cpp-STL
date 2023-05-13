3b0c6334d85c6e2a1c830c4506b6cf73ee6da93d
Ashwin Abraham <210050023@iitb.ac.in>
2023-05-14 04:12:48 +0530

    Transferring files

--- sort/test.cpp
+++ sort/test.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <chrono>
-#include "sort.hpp"
+#include <algorithm>
+#include <execution>
 
-constexpr std::size_t SIZE = 100'000'000;
+
+constexpr std::size_t SIZE = 1'000'000'000;
 constexpr std::size_t RANGE = 3001;
 
 int main(int argc, char ** argv)
@@ -11,7 +13,7 @@ int main(int argc, char ** argv)
     int *arr = new int[SIZE];
     for(int i = 0; i < SIZE; ++i) arr[i] = rand() % RANGE;
     auto t1 = std::chrono::high_resolution_clock::now();
-    sort::bubblesort(arr, SIZE);
+    std::sort(std::execution::par_unseq, arr, arr + SIZE);
     auto t2 = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration<double, std::milli>(t2 - t1).count() << '\n';
     delete [] arr;
